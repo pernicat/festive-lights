b94d2d8f4fd07131c57eb0138bdd699afd2fa0e5
Tony Pernicano <pernicat@gmail.com>
2018-12-08 19:17:17 -0500

    disables auto_write

--- festive.py
+++ festive.py
@@ -6,7 +6,7 @@ from festive import seasons
 
 LED_COUNT = int(os.getenv('LED_COUNT', 150))
 
-pixels = neopixel.NeoPixel(board.D18, LED_COUNT)
+pixels = neopixel.NeoPixel(board.D18, LED_COUNT, auto_write=False)
 
 while True:
     seasons.christmas(pixels)
